3faed7d0b837bb3a49e838ee219bec0643646b35
delwerHossain <delwerhossain006@gmail.com>
2023-04-18 13:59:26 +0100

    login page and firebase added

---
+++ m-61/ema-john/src/firebase/firebase.init.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAiVvTrfCFtdRc9a5Z0XAoC-P-d2Qu6fe8",
+  authDomain: "ema-john-auth-context-a993e.firebaseapp.com",
+  projectId: "ema-john-auth-context-a993e",
+  storageBucket: "ema-john-auth-context-a993e.appspot.com",
+  messagingSenderId: "190427695689",
+  appId: "1:190427695689:web:7953d6f60666f622b60d98"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
