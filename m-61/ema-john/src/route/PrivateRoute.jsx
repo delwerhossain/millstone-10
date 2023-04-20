a83ffdd17b5e83bd150e239a8e01d0ca6d17dc96
delwerHossain <delwerhossain006@gmail.com>
2023-04-20 09:51:32 +0100

    text-center

--- m-61/ema-john/src/route/PrivateRoute.jsx
+++ m-61/ema-john/src/route/PrivateRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ children }) => {
     const { user, loader } = useContext(AuthContext)
     if (loader) {
-        return <div className=' items-center my-8 flex justify-center'><div><h1 className='text-2xl mt-3'>loading...</h1> <progress className=" progress w-56"></progress></div></div>
+        return <div className=' items-center my-8 flex justify-center'><div><h1 className='text-2xl mt-3 text-center'>loading...</h1> <progress className=" progress w-56"></progress></div></div>
     }
     if (user) {
         return children
