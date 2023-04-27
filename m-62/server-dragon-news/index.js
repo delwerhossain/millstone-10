d51ca80c287c9df84fe9714fc2201cb7c1ccaefd
Delwer Hossain <delwerhossain006@gmail.com>
2023-04-28 02:55:02 +0600

    category added from express port

--- m-62/server-dragon-news/index.js
+++ m-62/server-dragon-news/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 const category =require('./category.json')
 
+app.use(cors())
 app.get('/', (req, res) => {
     res.send('hello drag')
 })
--- m-62/the-dragon-news/src/Components/Category/Category.jsx
+++ m-62/the-dragon-news/src/Components/Category/Category.jsx
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Category = () => {
+  const [category, setCategory] = useState([])
+  useEffect(() => {
+    const url = "http://localhost:3000/category";
+
+    // Create async function 
+    const fetchCategory = async () => {
+        try {
+            const response = await fetch(url);
+            const jsonData = await response.json();
+            setCategory(jsonData);
+        } catch (error) {
+            console.log("error fetchCategory :", error);
+        }
+    };
+
+    // Call fetch function 
+    fetchCategory();
+  }, []);
+  console.log(category);
   return (
     <div>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo magni
-        cupiditate iste.
-      </h1>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo magni
-        cupiditate iste.
-      </h1>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo magni
-        cupiditate iste.
-      </h1>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo magni
-        cupiditate iste.
-      </h1>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo magni
-        cupiditate iste.
-      </h1>
+      {category.map((cate) => <h1 className="lg:text-xl font-semibold mb-2">{cate.name}</h1>)}
     </div>
   );
 };
