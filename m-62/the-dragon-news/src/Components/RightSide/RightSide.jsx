5b5598ccd9a52d10f7b42e0aad158832eeb0ddf5
delwerHossain <delwerhossain006@gmail.com>
2023-04-27 11:46:02 +0100

    added

--- m-62/the-dragon-news/src/Components/RightSide/RightSide.jsx
+++ m-62/the-dragon-news/src/Components/RightSide/RightSide.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { IconContext } from "react-icons";
 import { FcGoogle } from "react-icons/fc";
-import { BsGithub } from "react-icons/bs";
+import { BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+import { GrFacebookOption } from "react-icons/gr";
 const RightSide = () => {
   return (
     <div>
       <IconContext.Provider value={{ size: "2em" }}>
         <div>
-          <h1 className="text-2xl font-semibold my-4 text-center">Login With</h1>
+          <h1 className="text-2xl font-semibold my-4 text-center">
+            Login With
+          </h1>
           <div className="grid gap-2 mx-2">
             <button className="flex gap-2 btn btn-outline btn-info">
               {" "}
@@ -16,11 +19,38 @@ const RightSide = () => {
             </button>
             {/* <h3> Lets go for a <FcGoogle />? </h3> */}
             <button className="flex gap-2 btn btn-outline ">
-              <BsGithub ></BsGithub> Login With Git
+              <BsGithub></BsGithub> Login With Git
             </button>
           </div>
         </div>
       </IconContext.Provider>
+      <div>
+        <h1 className="text-2xl font-semibold my-4 text-center">Find Us On</h1>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <tbody className="text-center">
+              <tr>
+                <th className="flex pl-16 p text-center items-center gap-2 font-semibold text-xl">
+                  <GrFacebookOption className="text-[#3B599C] w-8 h-8 p-1  rounded-full bg-slate-300" />{" "}
+                  <span className="mx-4 text-center">Facebook</span>
+                </th>
+              </tr>
+              <tr>
+                <th className="flex pl-16 p text-center items-center gap-2 font-semibold text-xl">
+                  <BsTwitter className="text-[#58A7DE] w-8 h-8 p-1  rounded-full bg-slate-300" />{" "}
+                  <span className="mx-4 text-center">Twitter</span>
+                </th>
+              </tr>
+              <tr>
+                <th className="flex pl-16 p text-center items-center gap-2 font-semibold text-xl">
+                  <BsInstagram className="w-8 h-8 p-1  rounded-full bg-slate-300" />{" "}
+                  <span className="mx-4 text-center">Instagram</span>
+                </th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
--- m-62/the-dragon-news/src/Pages/shared/Navbar.jsx
+++ m-62/the-dragon-news/src/Pages/shared/Navbar.jsx
@@ -31,7 +31,8 @@ const Navbar = () => {
     <>
       {/* title bar */}
       <div className="my-8 flex flex-col items-center">
-        <img className=" lg:w-96 w-auto " src={logo} alt="" />
+        <img className=" lg:w-3/12 mx-auto
+         w-auto " src={logo} alt="" />
         <h2 className="mt-4 text-[#706F6F] text-2xl">
           Journalism Without Fear or Favour
         </h2>
@@ -39,7 +40,7 @@ const Navbar = () => {
         <h3 className="mt-4 text-[#5c5959] text-xl">{moment().format('dddd, MMMM D, YYYY')}</h3>
       </div>
 
-      <div className=" h-20 w-full px-20 flex justify-center items-center gap-4 bg-[#F3F3F3]">
+      <div className=" h-20 w-11/12 mx-auto px-3 flex justify-center items-center gap-4 bg-[#F3F3F3]">
         <div className="">
           {" "}
           <button className="bg-[#D72050] px-8 py-4 text-white font-semibold">
