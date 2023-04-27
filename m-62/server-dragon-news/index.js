d93f646a94763b466321941b13735924d29036e0
Delwer Hossain <delwerhossain006@gmail.com>
2023-04-28 02:24:08 +0600

    json data added

--- m-62/server-dragon-news/index.js
+++ m-62/server-dragon-news/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const category =require('./category.json')
+
 app.get('/', (req, res) => {
     res.send('hello drag')
+})
+app.get('/category', (req, res) => {
+    res.send(category)
 })
 app.listen(port, () => {
     console.log(`Dragon is running  ${port}`);
