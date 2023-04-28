6a819ce3782fdcf123f2c99a98e1e6aeefe82ab1
delwerHossain <delwerhossain006@gmail.com>
2023-04-28 12:49:37 +0100

    server category and news is api ready

--- m-62/server-dragon-news/index.js
+++ m-62/server-dragon-news/index.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 3000
 const category =require('./category.json')
+const news =require('./news.json')
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -11,6 +12,24 @@ app.get('/', (req, res) => {
 app.get('/category', (req, res) => {
     res.send(category)
 })
+app.get('/news', (req, res) => {
+    res.send(news)
+ })
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selectedNews = news.find(news => news._id === id)
+    res.send(selectedNews)
+ })
+app.get('/category/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const selectedByCategory = news.filter(news => parseInt(news.category_id) === id)
+        res.send(selectedByCategory)
+    }
+ })
 app.listen(port, () => {
     console.log(`Dragon is running  ${port}`);
  })
